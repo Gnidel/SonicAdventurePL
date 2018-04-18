e3506229eb4369573bbc2d58446e0b02ba41b4e2
Gnidel <bargnia1@student.pg.gda.pl>
2018-04-18 03:09:47 +0200

    Dodano teksty spoza filmików oraz z brakujących filmików.

--- podmiana_znakow_pl.ps1
+++ podmiana_znakow_pl.ps1
@@ -4,7 +4,7 @@
 
 foreach($line in Get-Content .\polskie_znaki.txt) {
     $polskiznak, $znakniecrashujacy = $line.Split(" ");
-    Get-ChildItem -recurse ".\Sonic Adventure PL\Misc\Cutscene Text\*\*\English.txt" |
+    Get-ChildItem -recurse ".\Sonic Adventure PL\Misc\*\*\*\English.txt" |
     ForEach-Object {
             $name = $_.fullname; 
             (get-content $name ) | 
